806a203b7f060f35a8695879318ec2fe6dcb3451
Avelcorvin <avelcorvin@gmail.com>
2025-05-25 12:58:22 +0300

    fixed BD name

--- src/databases/mysql/index.js
+++ src/databases/mysql/index.js
@@ -13,7 +13,7 @@ class MySqlDB {
             host: '127.0.0.1',
             user: 'root',
             password: 'my-secret-pw',
-            database: 'MyBd',
+            database: 'mysql',
         });
         connection.connect();
         return connection;
--- src/databases/mysql/index.ts
+++ src/databases/mysql/index.ts
@@ -9,7 +9,7 @@ class MySqlDB{
       host     : '127.0.0.1',
       user     : 'root',
       password : 'my-secret-pw',
-      database : 'MyBd',
+      database : 'mysql',
     });
     
     connection.connect();
